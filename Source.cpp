bd3c4d14b35961cdb9fe8252eeabe495e700ad35
dadas190 <d.cernak@pobox.sk>
2017-07-08 14:44:11 +0200

    Fixed example code error

--- Source.cpp
+++ Source.cpp
@@ -9,11 +9,11 @@
 int main()
 {	
 	uint64_t user32 = LoadLibrary64("user32.dll");
-	uint64_t MessageBoxA = GetProcAddress64(user32, "MessageBoxA");
+	uint64_t MessageBoxA = GetProcAddress64(user32, (uint64_t)"MessageBoxA");
 	X64Call(MessageBoxA, 0, (uint64_t)"testing", (uint64_t)"msg", 0);
 
 	uint64_t kernel32 = GetModuleHandle64(L"kernel32.dll");
-	uint64_t TerminateProcess = GetProcAddress64(kernel32, "TerminateProcess");
+	uint64_t TerminateProcess = GetProcAddress64(kernel32, (uint64_t)"TerminateProcess");
 	X64Call(TerminateProcess, -1, 0x69, 0, 0);
 	return 0;
 }
